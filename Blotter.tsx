b36071583600bfd2bfb96a81c44eb647bf23a89d
tim <tim-zhang@live.com>
2026-06-20 13:25:17 -0600

    Tbale skeleton

--- Blotter.tsx
+++ Blotter.tsx
@@ -9,23 +9,41 @@ export default function Blotter() {
     const ws = new WebSocket('wss://advanced-trade-ws.coinbase.com');
     ws.onopen = () => {
     // Only subscribe AFTER the connection is open
-    ws.send(JSON.stringify({
-        type: 'subscribe',
-        product_ids: ['LTC-USD'],
-        channel: 'level2'
-    }));
+      ws.send(JSON.stringify({
+          type: 'subscribe',
+          product_ids: ['LTC-USD'],
+          channel: 'level2'
+      }));
     };
-    ws.onmessage = (event: MessageEvent) => {
-    const message = JSON.parse(event.data);
-    //@ts-ignore
-    display.current.innerText = event.data;
+      ws.onmessage = (event: MessageEvent) => {
+      const message = JSON.parse(event.data);
+      //@ts-ignore
+      display.current.innerText = event.data;
+
     };
   })
 
+
+
   return <div>
-
-    <p ref={display}> </p>
-
+    <table ref={display}>
+      <thead>
+        <tr>
+          <th>Product</th>
+          <th>Price</th>
+          <th>Side</th>
+          <th>Quantity</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>LTC-USD</td>
+          <td>100</td>
+          <td>Offer</td>
+          <td>100</td>
+        </tr>
+      </tbody>
+   </table>
   </div>
 }
 
